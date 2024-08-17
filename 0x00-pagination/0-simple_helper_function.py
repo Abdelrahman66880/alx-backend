7e2713a41eb4b8903fe218c332af34678deea0fe
Abdelrahman <abdofathi317@gmail.com>
2024-08-16 17:18:17 -0700

    Simple pagination

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -2,10 +2,12 @@
 """
 Write a function that takes two arguments
 page, page_size
-this function returns tuple of size two containing 
-a start index and an end index corresponding to 
-the range of indexes to return in a list for those 
-particular pagination parameters.
+
+this function returns tuple of size two containing
+a start index and an end index corresponding to
+
+the range of indexes to return in a list for
+those particular pagination parameters.
 """
 
 from typing import Tuple
